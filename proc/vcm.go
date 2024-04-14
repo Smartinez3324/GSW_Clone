a71b8476f62da7b095ef768335437da25afc7fac
Aaron Chan <aarchan108@gmail.com>
2024-04-13 22:00:26 -0700

    Define structs for packet info and fields

--- proc/vcm.go
+++ proc/vcm.go
@@ -1,1 +1,21 @@
 package proc
+
+import (
+	"encoding/binary"
+)
+
+// TelemetryPacketInfo Information about a telemetry packet
+type TelemetryPacketInfo struct {
+	name   string               // Name of the telemetry packet
+	port   uint16               // Port number of the telemetry packet
+	fields []TelemetryFieldInfo // Information about the fields in the telemetry packet
+}
+
+// TelemetryFieldInfo Information about a telemetry field in a telemetry packet
+type TelemetryFieldInfo struct {
+	name       string           // Name of the telemetry data
+	size       uint16           // Size of the telemetry data in bytes
+	padding    uint16           // Number of padding bytes
+	endianness binary.ByteOrder // Endianess of the telemetry data
+	signed     bool             // Whether the telemetry data is signed
+}

5e15c86791b669707d9dfb8ad937644fba2277e4
Aaron Chan <aarchan108@gmail.com>
2024-04-13 23:48:29 -0700

    Convert at port level

--- proc/vcm.go
+++ proc/vcm.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 type TelemetryPacket struct {
 	Fields map[string]TelemetryPacketField
+	Port   uint16
 }
 
 type TelemetryPacketField struct {
@@ -26,6 +28,7 @@ type telemetryConfigurationField struct {
 }
 
 func ParseConfiguration(filename string) []TelemetryPacket {
+	// Put file data into memory
 	file, _ := os.Open(filename)
 
 	defer func(file *os.File) {
@@ -37,6 +40,7 @@ func ParseConfiguration(filename string) []TelemetryPacket {
 
 	jsonStr, _ := io.ReadAll(file)
 
+	// Convert JSON into map of telemetryConfigurations
 	var config map[string]telemetryConfiguration
 
 	err := json.Unmarshal(jsonStr, &config)
@@ -45,6 +49,29 @@ func ParseConfiguration(filename string) []TelemetryPacket {
 		return nil
 	}
 
+	// Convert to internal data structure
 	var packets []TelemetryPacket
+	convertConfiguration(config)
+
+	return packets
+}
+
+func convertConfiguration(config map[string]telemetryConfiguration) []TelemetryPacket {
+	// Allocate a slice of TelemetryPackets
+	packets := make([]TelemetryPacket, 0)
+
+	fmt.Println("Processing", len(config), "ports")
+	for key, value := range config {
+		fmt.Println("Processing port", key)
+		port, _ := strconv.Atoi(key)
+		packets = append(packets, TelemetryPacket{
+			Port: uint16(port),
+		})
+
+		fmt.Println(value)
+	}
+
+	fmt.Println(packets)
+
 	return packets
 }

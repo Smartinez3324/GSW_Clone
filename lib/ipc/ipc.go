8a51511432339f9e276ea12743fc0622c6aa7197
Aaron Chan <aarchan108@gmail.com>
2024-07-19 23:55:33 -0700

    Shm service init code and stubs

--- lib/ipc/ipc.go
+++ lib/ipc/ipc.go
@@ -1,8 +1,15 @@
 package ipc
 
-type IpcHandler interface {
-	Setup() error
+import "github.com/AarC10/GSW-V2/proc"
+
+type IpcServiceSide interface {
+	Setup(packet proc.TelemetryPacket) error
 	Cleanup() error
-	Write(id int, data []byte) error
-	Read(id int) ([]byte, error)
+	Write(data []byte) error
+}
+
+type IpcClientSide interface {
+	Setup(packet proc.TelemetryPacket) error
+	Cleanup() error
+	Read() ([]byte, error)
 }
--- lib/ipc/shm.go
+++ lib/ipc/shm.go
@@ -1,24 +1,80 @@
 package ipc
 
-type ShmHandler struct {
+import (
+	"fmt"
+	"github.com/AarC10/GSW-V2/proc"
+	"github.com/cloudwego/shmipc-go"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"syscall"
+)
+
+type ShmCommon struct {
+	unixListener *net.UnixListener
+	conn         net.Conn
+	stream       *shmipc.Stream
 }
 
-func (shmHandler *ShmHandler) Setup() error {
+type ShmServiceSide struct {
+	common    ShmCommon
+	shmServer *shmipc.Session
+}
+
+type ShmClientSide struct {
+	common    ShmCommon
+	shmClient *shmipc.Session
+}
+
+func (shmHandler *ShmServiceSide) Setup(telemetryPacket proc.TelemetryPacket) error {
+	// Setup Unix domain socket
+	udsPath := filepath.Join(os.TempDir(), "gsw-service-", telemetryPacket.Name, "-", strconv.Itoa(telemetryPacket.Port), ".uds_sock")
+	_ = syscall.Unlink(udsPath)
+	unixListener, err := net.ListenUnix("unix", &net.UnixAddr{Name: udsPath, Net: "unix"})
+	if err != nil {
+		return fmt.Errorf("Creating Unix domain socket failed: %v", err)
+	}
+
+	// Accept UDS
+	conn, err := unixListener.Accept()
+	if err != nil {
+		return fmt.Errorf("Accepting Unix domain socket failed: %v", err)
+	}
+	shmHandler.common.unixListener = unixListener
+
+	// Create server session
+	conf := shmipc.DefaultConfig()
+	shmServer, err := shmipc.Server(conn, conf)
+	if err != nil {
+		return fmt.Errorf("IPC server creation failed: %v", err)
+	}
+	shmHandler.common.conn = conn
+	shmHandler.shmServer = shmServer
+
+	// Accept stream
+	stream, err := shmServer.AcceptStream()
+	if err != nil {
+		return fmt.Errorf("Accept stream failed: %v", err)
+	}
+	shmHandler.common.stream = stream
 
 	return nil
 }
 
-func (shmHandler *ShmHandler) Cleanup() error {
+func (shmHandler *ShmServiceSide) Cleanup() error {
+	shmHandler.common.unixListener.Close()
+	shmHandler.common.conn.Close()
+	shmHandler.shmServer.Close()
+	shmHandler.common.stream.Close()
+	return nil
+}
+
+func (shmHandler *ShmServiceSide) Write(data []byte) error {
 
 	return nil
 }
 
-func (shmHandler *ShmHandler) Write(id int, data []byte) error {
-
-	return nil
-}
-
-func (shmHandler *ShmHandler) Read(id int) ([]byte, error) {
-
+func (shmHandler *ShmClientSide) Read() ([]byte, error) {
 	return nil, nil
 }

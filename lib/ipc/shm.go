3388387e959dfa7ffb8390f75b228c865cab7ff3
Aaron Chan <aarchan108@gmail.com>
2024-07-20 01:29:55 -0700

    Stubbed shm file

--- lib/ipc/shm.go
+++ lib/ipc/shm.go
@@ -1,1 +1,42 @@
 package ipc
+
+import (
+	"github.com/AarC10/GSW-V2/proc"
+)
+
+type IpcShmCommon struct {
+}
+
+type IpcShmWriter struct {
+	common IpcShmCommon
+}
+
+type IpcShmReader struct {
+	common IpcShmCommon
+}
+
+func (shmHandler *IpcShmWriter) Setup(telemetryPacket proc.TelemetryPacket) error {
+
+	return nil
+}
+
+func (shmHandler *IpcShmWriter) Cleanup() {
+
+}
+
+func (shmHandler *IpcShmReader) Setup(packet proc.TelemetryPacket) error {
+	return nil
+}
+
+func (shmHandler *IpcShmReader) Cleanup() {
+
+}
+
+func (shmHandler *IpcShmWriter) Write(data []byte) error {
+
+	return nil
+}
+
+func (shmHandler *IpcShmReader) Read() ([]byte, error) {
+	return nil, nil
+}
